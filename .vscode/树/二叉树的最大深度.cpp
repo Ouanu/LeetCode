49c1c414277d04dd1f7860ab8469f80c506b40ea
Ouanu <ouanu@foxmail.com>
2021-09-24 16:41:05 +0800

    master

---
+++ .vscode/树/二叉树的最大深度.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        if(root->left == nullptr && root->right == nullptr) return 1;
+        int maxRightDepth = maxDepth(root->right);
+        int maxLeftDepth = maxDepth(root->left);
+        if(maxLeftDepth < maxRightDepth){
+            return maxRightDepth + 1;
+        }else{
+            return maxLeftDepth + 1;
+        }
+    }
+};
