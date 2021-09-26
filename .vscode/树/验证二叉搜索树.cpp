172370463ad84dd4ba32aba84ed2c3dd2809ef3e
Ouanu <ouanu@foxmail.com>
2021-09-26 20:50:18 +0800

    master

--- .vscode/树/验证二叉搜索树.cpp
+++ .vscode/树/验证二叉搜索树.cpp
@@ -9,16 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    bool isValidBST(TreeNode* root) {
+    bool isValidBST(TreeNode *root)
+    {
         return isValidBST(root, LONG_MIN, LONG_MAX);
     }
-    bool isValidBST(TreeNode* root, long minVal, long maxVal){
-        if(root == nullptr) return true;
-        if(root->val >= maxVal || root->val <= minVal)return false;
-
-        return isValidBST(root->left, minVal, root->val)&&
-        isValidBST(root->right, root->val, maxVal);
+    bool isValidBST(TreeNode *root, long minVal, long maxVal)
+    {
+        if (root == nullptr)
+            return true;
+        //每个节点如果超过这个范围，直接返回false
+        if (root->val >= maxVal || root->val <= minVal)
+            return false;
+        //这里再分别以左右两个子节点分别判断，
+        //左子树范围的最小值是minVal，最大值是当前节点的值，也就是root的值，因为左子树的值要比当前节点小
+        //右子数范围的最大值是maxVal，最小值是当前节点的值，也就是root的值，因为右子树的值要比当前节点大
+        return isValidBST(root->left, minVal, root->val) &&
+               isValidBST(root->right, root->val, maxVal);
     }
 };
